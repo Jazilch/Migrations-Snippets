c2eb3a8f2e30ce759f35d000de57d14000628622
Jazilch <zilchjames5@gmail.com>
2017-10-02 20:50:19 -0400

    Create example.js

---
+++ Match-Height/example.js
@@ -0,0 +1,8 @@
+<script src="//cdn2.hubspot.net/hubfs/1700416/Brevet%20August2017%20Theme/JS/imagesloaded.pkgd.min.js"></script>
+<script src="//cdn2.hubspot.net/hubfs/1700416/Brevet%20August2017%20Theme/JS/jquery.matchHeight.js"></script>
+<script>
+$('.post-item .inner-post').matchHeight({
+    byRow: true,
+    property: 'height'
+});
+</script>
